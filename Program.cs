50d309d8bd4e610d83455e4d30f95088c15c30e4
Konstantin <alx0000@yandex.ru>
2015-09-28 23:00:35 +0300

    Убрал нахрен задачи

--- Program.cs
+++ Program.cs
@@ -10,7 +10,8 @@ namespace AclGen
 {
     internal class Program
     {
-       static Logger log = LogManager.GetCurrentClassLogger();
+        private static readonly Logger log = LogManager.GetCurrentClassLogger();
+
         private static void Main(string[] args)
         {
             var settings = new
@@ -19,59 +20,41 @@ namespace AclGen
                 CompName = ConfigurationSettings.AppSettings["ComputerName"],
                 NameFile = ConfigurationSettings.AppSettings["FileName"]
             };
-            
+
             log.Debug(
                 $"Apply Settings: {nameof(settings.CountUserGen)} ={settings.CountUserGen}, {nameof(settings.CompName)}={settings.CompName}, {nameof(settings.NameFile)} ={settings.NameFile}");
 
-
-            MainAsync(settings.CountUserGen, settings.CompName, settings.NameFile).Wait();
-        }
-
-        private static async Task MainAsync(int Count, string compName, string file)
-        {
-            var users = Enumerable.Range(1, Count)
-                .Select(n => "user" + n).ToList();
+            var users = Enumerable.Range(1, settings.CountUserGen)
+               .Select(n => "user" + n).ToList();
 
             var sw = Stopwatch.StartNew();
-            var tasks =
-                users
-                    .Select(async n => await RunProcessAsync("net", "user " + n + " Pa$$worD /ADD")).ToList();
 
-            var results = await Task.WhenAll(tasks);
+            users
+                .ForEach(n =>
+                {
+                    var p = new Process
+                    {
+                        StartInfo =
+                        {
+                            WindowStyle = ProcessWindowStyle.Hidden,
+                            FileName = "net",
+                            Arguments = $"user {n} Pa$$worD /ADD"
+                        }
+                    };
+                    p.Start();
+                    p.WaitForExit();
+                });
+
             sw.Stop();
             log.Debug($"Creating users took {sw.Elapsed}");
             sw.Start();
             users.ForEach(
                 login =>
-                    AddDirectorySecurity(file, compName+ @"\" + login, FileSystemRights.FullControl,
+                    AddDirectorySecurity(settings.NameFile, settings.CompName + @"\" + login, FileSystemRights.FullControl,
                         AccessControlType.Allow));
             sw.Stop();
             log.Debug($"Setting rights took {sw.Elapsed}");
         }
-
-        // TODO : Стоит добавить анализ через bool создалась ли учетка возможно...
-        private static Task<bool> RunProcessAsync(string fileName, string arg)
-        {
-            // there is no non-generic TaskCompletionSource
-            var tcs = new TaskCompletionSource<bool>();
-
-            var process = new Process
-            {
-                StartInfo = {FileName = fileName, Arguments = arg, WindowStyle = ProcessWindowStyle.Hidden },
-                EnableRaisingEvents = true
-            };
-
-            process.Exited += (sender, args) =>
-            {
-                tcs.SetResult(true);
-                process.Dispose();
-            };
-
-            process.Start();
-
-            return tcs.Task;
-        }
-
         // Adds an ACL entry on the specified directory for the specified account.
         public static void AddDirectorySecurity(string FileName, string Account, FileSystemRights Rights,
             AccessControlType ControlType)
