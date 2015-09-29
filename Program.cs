711236018813a62bbcbb7cd615464164ba624d89
Konstantin <alx0000@yandex.ru>
2015-09-29 07:22:39 +0300

    Убарал из конфига имя компа, перевел создание пользователей на дотнетовский класс

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System.Configuration;
+﻿using System;
+using System.Configuration;
 using System.Diagnostics;
+using System.DirectoryServices;
 using System.IO;
 using System.Linq;
 using System.Security.AccessControl;
-using System.Threading.Tasks;
 using NLog;
 
 namespace AclGen
@@ -16,63 +17,47 @@ namespace AclGen
         {
             var settings = new
             {
-                CountUserGen = int.Parse(ConfigurationSettings.AppSettings["CountUserGen"]),
-                CompName = ConfigurationSettings.AppSettings["ComputerName"],
-                NameFile = ConfigurationSettings.AppSettings["FileName"]
+                CountUserGen = int.Parse(ConfigurationManager.AppSettings["CountUserGen"]),
+                NameFile = ConfigurationManager.AppSettings["FileName"]
             };
 
             log.Debug(
-                $"Apply Settings: {nameof(settings.CountUserGen)} ={settings.CountUserGen}, {nameof(settings.CompName)}={settings.CompName}, {nameof(settings.NameFile)} ={settings.NameFile}");
+                $"Apply Settings: {nameof(settings.CountUserGen)} ={settings.CountUserGen}, {nameof(Environment.MachineName)}={Environment.MachineName}, {nameof(settings.NameFile)} ={settings.NameFile}");
 
             var users = Enumerable.Range(1, settings.CountUserGen)
-               .Select(n => "user" + n).ToList();
+                .Select(n => "user" + n).ToList();
 
             var sw = Stopwatch.StartNew();
 
             users
                 .ForEach(n =>
                 {
-                    var p = new Process
-                    {
-                        StartInfo =
-                        {
-                            WindowStyle = ProcessWindowStyle.Hidden,
-                            FileName = "net",
-                            Arguments = $"user {n} Pa$$worD /ADD"
-                        }
-                    };
-                    p.Start();
-                    p.WaitForExit();
+
+                    // Возможно надо обернуть в юзинги?
+                    var ad = new DirectoryEntry("WinNT://" +
+                                                Environment.MachineName + ",computer");
+
+                    var newUser = ad.Children.Add(n, "user");
+
+                    newUser.Invoke("SetPassword", "Pa$$worD");
+                    newUser.CommitChanges();
                 });
 
             sw.Stop();
             log.Debug($"Creating users took {sw.Elapsed}");
+
+            var dInfo = new DirectoryInfo(settings.NameFile);
+            var dSecurity = dInfo.GetAccessControl();
+
             sw.Start();
             users.ForEach(
                 login =>
-                    AddDirectorySecurity(settings.NameFile, settings.CompName + @"\" + login, FileSystemRights.FullControl,
-                        AccessControlType.Allow));
+                    dSecurity.AddAccessRule(new FileSystemAccessRule(login, FileSystemRights.FullControl,
+                        AccessControlType.Allow))
+                );
+            dInfo.SetAccessControl(dSecurity);
             sw.Stop();
             log.Debug($"Setting rights took {sw.Elapsed}");
-        }
-        // Adds an ACL entry on the specified directory for the specified account.
-        public static void AddDirectorySecurity(string FileName, string Account, FileSystemRights Rights,
-            AccessControlType ControlType)
-        {
-            // Create a new DirectoryInfo object.
-            var dInfo = new DirectoryInfo(FileName);
-
-            // Get a DirectorySecurity object that represents the 
-            // current security settings.
-            var dSecurity = dInfo.GetAccessControl();
-
-            // Add the FileSystemAccessRule to the security settings. 
-            dSecurity.AddAccessRule(new FileSystemAccessRule(Account,
-                Rights,
-                ControlType));
-
-            // Set the new access settings.
-            dInfo.SetAccessControl(dSecurity);
         }
     }
 }
